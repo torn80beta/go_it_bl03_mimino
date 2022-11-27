c0d39730abc9f72e8e6edb1d883ce90772bb2655
torn80beta <103459735+torn80beta@users.noreply.github.com>
2022-11-27 19:42:36 +0200

    slider

--- src/js/slick.js
+++ src/js/slick.js
@@ -23,9 +23,10 @@ $('.variable-width').slick({
   prevArrow: '<button type="button" class="my-slick-prev">&xlarr;</button>',
   nextArrow: '<button type="button" class="my-slick-next">&xrarr;</button>',
   dots: false,
+  appendArrows: $('.slider-nav'),
   infinite: true,
   speed: 300,
-  slidesToShow: 3,
+  slidesToShow: 2,
   centerMode: true,
   variableWidth: true
 });
