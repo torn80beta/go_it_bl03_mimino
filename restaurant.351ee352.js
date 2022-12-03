ea7d2836e310dd74dca567ae65f12bf6c2538bde
torn80beta <103459735+torn80beta@users.noreply.github.com>
2022-12-03 18:26:46 +0000

    Deploying to gh-pages from @ torn80beta/go_it_bl03_mimino@1bddd7860b26e04cafd06769b60eb2141ddfa301 🚀

---
+++ restaurant.351ee352.js
@@ -0,0 +1,2 @@
+!function(){var e={openModalBtn:document.querySelector("[data-modal-table-open]"),closeModalBtn:document.querySelector("[data-modal-table-close]"),modal:document.querySelector("[data-modal-table]")};function o(){e.modal.classList.toggle("is-hidden")}e.openModalBtn.addEventListener("click",o),e.closeModalBtn.addEventListener("click",o)}();
+//# sourceMappingURL=restaurant.351ee352.js.map
