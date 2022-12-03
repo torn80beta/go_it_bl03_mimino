c35582b097d22880b930181c6ae6c1a8811b03ae
Kirill Voronin <voroninkv09@gmail.com>
2022-12-03 17:40:08 +0200

    add changes

--- src/js/button_manager.js
+++ src/js/button_manager.js
@@ -25,10 +25,6 @@
   mainRightButtons.addEventListener('click', function () {
     firstSliderButtonNext.click();
     secondSliderButtonNext.click();
-
-    // firstSlider.addEventListener('slickNext', function () {
-    //   secondSliderButtonNext.click();
-    // });
   });
 
   (function () {
@@ -39,39 +35,69 @@
   })();
 })();
 
+// (function () {
+//   let firstSlider = document.querySelector(
+//     '.first-slider-details-page-restaurant'
+//   );
+//   let firstSliderButtonNext = firstSlider.querySelector('.slick-next');
+//   let firstSliderButtonPrev = firstSlider.querySelector('.slick-prev');
+
+//   let secondSlider = document.querySelector(
+//     '.second-slider-details-page-restaurant'
+//   );
+//   let secondSliderButtonNext = secondSlider.querySelector('.slick-next');
+//   let secondSliderButtonPrev = secondSlider.querySelector('.slick-prev');
+
+//   let mainLeftButtons = document.querySelector(
+//     '.restaurant-details-slider__button--left'
+//   );
+//   let mainRightButtons = document.querySelector(
+//     '.restaurant-details-slider__button--right'
+//   );
+
+//   mainLeftButtons.addEventListener('click', function () {
+//     firstSliderButtonPrev.click();
+//     secondSliderButtonPrev.click();
+//   });
+//   mainRightButtons.addEventListener('click', function () {
+//     firstSliderButtonNext.click();
+//     secondSliderButtonNext.click();
+//   });
+
+//   (function () {
+//     firstSliderButtonPrev.classList.toggle('is-hidden');
+//     firstSliderButtonNext.classList.toggle('is-hidden');
+//     secondSliderButtonPrev.classList.toggle('is-hidden');
+//     secondSliderButtonNext.classList.toggle('is-hidden');
+//   })();
+// })();
 (function () {
-  let firstSlider = document.querySelector(
-    '.first-slider-details-page-restaurant'
+  let thirdSlider = document.querySelector(
+    '.restaurant-details__wrapper-slider-three-cards'
   );
-  let firstSliderButtonNext = firstSlider.querySelector('.slick-next');
-  let firstSliderButtonPrev = firstSlider.querySelector('.slick-prev');
+  let thirdSliderButtonNext = thirdSlider.querySelector('.slick-next');
+  let thirdSliderButtonPrev = thirdSlider.querySelector('.slick-prev');
 
-  let secondSlider = document.querySelector(
-    '.second-slider-details-page-restaurant'
-  );
-  let secondSliderButtonNext = secondSlider.querySelector('.slick-next');
-  let secondSliderButtonPrev = secondSlider.querySelector('.slick-prev');
-
-  let mainLeftButtons = document.querySelector(
+  let mainLeftButtons = thirdSlider.querySelector(
     '.restaurant-details-slider__button--left'
   );
-  let mainRightButtons = document.querySelector(
+  let mainRightButtons = thirdSlider.querySelector(
     '.restaurant-details-slider__button--right'
   );
 
   mainLeftButtons.addEventListener('click', function () {
-    firstSliderButtonPrev.click();
-    secondSliderButtonPrev.click();
+    thirdSliderButtonPrev.click();
+    // secondSliderButtonPrev.click();
   });
   mainRightButtons.addEventListener('click', function () {
-    firstSliderButtonNext.click();
-    secondSliderButtonNext.click();
+    thirdSliderButtonNext.click();
+    // secondSliderButtonNext.click();
   });
 
   (function () {
-    firstSliderButtonPrev.classList.toggle('is-hidden');
-    firstSliderButtonNext.classList.toggle('is-hidden');
-    secondSliderButtonPrev.classList.toggle('is-hidden');
-    secondSliderButtonNext.classList.toggle('is-hidden');
+    thirdSliderButtonPrev.classList.toggle('is-hidden');
+    thirdSliderButtonNext.classList.toggle('is-hidden');
+    // secondSliderButtonPrev.classList.toggle('is-hidden');
+    // secondSliderButtonNext.classList.toggle('is-hidden');
   })();
 })();
--- src/js/slider_three_images.js
+++ src/js/slider_three_images.js
@@ -7,3 +7,12 @@
 //   variableWidth: true,
 //   // rtl: true,
 // });
+$('.restaurant-details__slider-third').slick({
+  dots: false,
+  infinite: true,
+  speed: 500,
+  slidesToShow: 1,
+  centerMode: true,
+  variableWidth: true,
+  // rtl: true,
+});
