a8b0efa80d58f928c8a383a03a073a048cad316d
Oleksandr Vedmedenko <stbearua@gmail.com>
2022-12-04 20:10:51 +0100

    slider+!

--- src/js/hero-slider.js
+++ src/js/hero-slider.js
@@ -2,15 +2,7 @@ $(document).ready(function () {
   $('.hero-slider').slick({
     infinite: true,
     arrows: true,
-    appendArrows: $('.hero-slider-arrows'),
     speed: 300,
     cssEase: 'linear',
   });
 });
-
-//
-// slidesToShow: 1,
-// variableWidth: true,
-// appendArrows: $('.hero-slider-nav'),
-// fade: true,
-// speed: 300,
