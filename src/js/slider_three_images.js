5c3fe103fc0551ca4e325c1099833d5fbbdb7afd
Kirill Voronin <voroninkv09@gmail.com>
2022-12-04 16:05:03 +0200

    add js

---
+++ src/js/slider_three_images.js
@@ -0,0 +1,9 @@
+$('.restaurant-details__slider-third').slick({
+  dots: false,
+  infinite: true,
+  speed: 500,
+  slidesToShow: 1,
+  centerMode: true,
+  variableWidth: true,
+  // rtl: true,
+});
