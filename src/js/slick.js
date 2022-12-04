a64d674d7caa18dcf8f2938868866a57eb84d9ce
torn80beta <103459735+torn80beta@users.noreply.github.com>
2022-12-04 19:45:53 +0200

    fckn slider

--- src/js/slick.js
+++ src/js/slick.js
@@ -5,5 +5,16 @@ $(document).ready(function () {
         variableWidth: true,
         appendArrows: $('.slider-nav'),
         speed: 300,
+        rtl: true,
+    });
+});
+
+$(document).ready(function () {
+    $('.triple-slider').slick({
+        arrows: true,
+        slidesToShow: 2,
+        variableWidth: true,
+        appendArrows: $('.slider-nav'),
+        speed: 300,
     });
 });
