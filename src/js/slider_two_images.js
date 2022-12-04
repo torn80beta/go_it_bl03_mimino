1ce1218686a5e53297c7bd5560b29985e87c7af5
Kirill Voronin <voroninkv09@gmail.com>
2022-12-04 17:34:19 +0200

    add sliders

--- src/js/slider_two_images.js
+++ src/js/slider_two_images.js
@@ -34,3 +34,26 @@ $('.restaurant-details-slider__second').slick({
   variableWidth: true,
   asNavFor: '.restaurant-details-slider__first',
 });
+
+$('.probe-slider').slick({
+  prevArrow: '<button type="button" class="probe-slider-prev"></button>',
+  nextArrow: '<button type="button" class="probe-slider-next"></button>',
+  dots: false,
+  appendArrows: $('.probe-slider__buttons'),
+  infinite: true,
+  speed: 300,
+  slidesToShow: 1,
+  centerMode: false,
+  variableWidth: true,
+});
+$('.probe-slider-triple').slick({
+  prevArrow: '<button type="button" class="probe-slider-triple-prev"></button>',
+  nextArrow: '<button type="button" class="probe-slider-triple-next"></button>',
+  dots: false,
+  appendArrows: $('.probe-slider-triple__buttons'),
+  infinite: true,
+  speed: 300,
+  slidesToShow: 2,
+  centerMode: true,
+  variableWidth: true,
+});
