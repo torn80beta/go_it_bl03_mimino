8b02b72c6c9952430c5d9aa03e6550c232ab8708
torn80beta <103459735+torn80beta@users.noreply.github.com>
2022-12-05 16:41:58 +0000

    Deploying to gh-pages from @ torn80beta/go_it_bl03_mimino@bf2df48ec37bf66cc8fd5196c0e49d21cbeb9865 🚀

---
+++ restaurant.fd0642f8.js
@@ -0,0 +1,2 @@
+document.addEventListener("DOMContentLoaded",(function(){var e=document.querySelector(".tel-input");IMask(e,{mask:"+{38} (000) 000-00-00"});IMask(document.querySelector(".tel-input"),{mask:"+{38} (000) 000-00-00",lazy:!1})}));
+//# sourceMappingURL=restaurant.fd0642f8.js.map
