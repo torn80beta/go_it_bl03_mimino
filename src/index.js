7726c06e5285e60f593887f2d4b83161dcda0804
Kirill Voronin <voroninkv09@gmail.com>
2022-12-05 07:58:59 +0200

    change html and css for restaurant details section

--- src/index.js
+++ src/index.js
@@ -2,6 +2,3 @@ import './js/slick';
 import './js/modal';
 import './js/modal-room';
 import './js/modal-table';
-import './js/slider_three_images';
-import './js/slider_two_images';
-import './js/button_manager';
--- src/js/slider_three_images.js
+++
@@ -1,9 +0,0 @@
-$('.restaurant-details__slider-third').slick({
-  dots: false,
-  infinite: true,
-  speed: 500,
-  slidesToShow: 1,
-  centerMode: true,
-  variableWidth: true,
-  // rtl: true,
-});
