a92c9a3642b6bd525e387e929553c0c587a979cf
Yuriy-risk <111949781+Yuriy-risk@users.noreply.github.com>
2022-12-06 02:15:35 +0200

    Modal_book_room done

--- src/index.js
+++ src/index.js
@@ -2,4 +2,6 @@ import './js/hero-slider';
 import './js/slick';
 import './js/modal-room';
 import './js/modal-table';
-import './js/modal-price'
+import './js/modal-price';
+import './js/imask';
+import './js/masked';
